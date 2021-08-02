d98a17659e9056352763196d41b9d284772e45dc
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-02 17:50:34 +0300

    updated run file

--- run.py
+++ run.py
@@ -10,6 +10,27 @@ def create_user(username, password):
     return new_user
 
 
+def sign_in(username, password):
+    user_exists = User.user_exist(username, password)
+    if sign_in != False:
+        return user_exists
+
+def save_user(user):
+    user.save_user()
+
+def create_details(username, password):
+    new_details = Details(username, password)
+    return new_details
+
+def save_details(details):
+    details.save_details()
+
+def display_detail_list():
+    return Details.display_details()
+
+def delete_details(details):
+    details.delete_details()
+
 
 if __name__ == '__main__':
     main()
