ddfde32dd92d3675cd96c7160c40174a03c6c8fe
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-02 17:45:41 +0300

    added test init function

--- test.py
+++ test.py
@@ -3,7 +3,18 @@ from user import User, Details
 
 class TestUser(unittest.TestCase):
 
-    
+    def setUp(self):
+
+        self.new_user = User('Collo', 'Collo2')
+
+    def test_init(self):
+
+        self.assertEqual(self.new_user.username,'Collo')
+        self.assertEqual(self.new_user.password,'Collo2')
+
+
+
+
 
 
 if __name__ == '__main__':
