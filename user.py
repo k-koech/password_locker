6c998d54275ef661cdf8574176a695d50a12f5d8
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-02 17:37:48 +0300

    created save user function in user

--- user.py
+++ user.py
@@ -4,3 +4,16 @@ class User:
     """
 
     user_list = []
+
+    def __init__(self,username,password):
+
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        save_user method saves a new user objects to the user_list
+        """
+        User.user_list.append(self)
+
+    
